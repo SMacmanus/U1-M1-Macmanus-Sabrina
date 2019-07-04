a2fa6d3f1d58161c933c941b7c3891f8129c81e2
SMacmanus <brinamac24@gmail.com>
2019-07-04 06:00:00 -0500

    fixed monthIf loop

--- src/main/java/MonthConverterIf.java
+++ src/main/java/MonthConverterIf.java
@@ -1,41 +1,19 @@
 import java.util.Scanner;
 
 public class MonthConverterIf {
-
     public static void main(String[] args) {
 
         String[] months = {"January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December"};
-
-        Scanner userInput = new Scanner(System.in);
+        String monthString;
 
         System.out.println("Please enter a whole number between 1 and 12.");
+        Scanner userInput = new Scanner(System.in);
         int input = userInput.nextInt();
 
-        if(input == 1) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 2) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 3) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 4) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 5) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 6) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 7) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 8) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 9) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input ==10) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input ==11) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
-        } else if(input == 12) {
-            System.out.println("Month number " + input + " corresponds with " + months[input - 1] + " ." );
+        if(input < months.length) {
+            monthString = months[input-1];
+            System.out.println(input + " corresponds with " + monthString);
         } else {
             System.out.println("You have entered an invalid number. " +
                     "You must enter a number between 1 and 12. Goodbye.");
